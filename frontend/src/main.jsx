6f2602ff2943fe7a5a673e782dfe5312ce7ef6f5
woosungking <abuga991008@gmail.com>
2025-05-14 21:07:19 +0900

    chore: 충돌난 부분 수정

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,29 +1,24 @@
-import React from 'react';
+// import App from './App';
+// import App from './App.jsx';
+// import OliveUserRegisterPage from './page/test/OliveUserRegisterPage.jsx'
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import App from './App.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { StrictMode } from 'react';
 import TestUserRegisterPage from './page/test/TestUserRegisterPage.jsx';
 import TestUserInfoPage from './page/test/TestUserInfoPage.jsx';
-import OliveUserRegisterPage from './page/test/OliveUserRegisterPage.jsx'
 import UserloginPage from './page/test/UserloginPage.jsx'
 
 
-const root = createRoot(document.getElementById('root'));
+// const root = createRoot(document.getElementById('root'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
   <StrictMode>
     <Router>
       <Routes>
         <Route path="/test/register" element={<TestUserRegisterPage/>}/>
         <Route path="/test/info" element={<TestUserInfoPage/>}/>
-        <Route path="/test/join" element={<OliveUserRegisterPage/>}/>
+        {/* <Route path="/test/join" element={<OliveUserRegisterPage/>}/> */}
         <Route path="/test/login" element={<UserloginPage/>}/>
       </Routes>
     </Router>
