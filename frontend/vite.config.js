0b076d4ebeca0bc7e66710caca1c311183b14909
woosungking <abuga991008@gmail.com>
2025-05-15 14:24:45 +0900

    chore

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,32 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
+// export default ({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), '');
+//   return defineConfig({
+//     plugins: [react(), tailwindcss()],
+//     server: {
+//       host: '0.0.0.0',
+//       port: 5173,
+//       proxy: {
+//         '/api/v1': {
+//           target: env.VITE_PROXY_ADDRESS,
+//           changeOrigin: true,
+//           rewrite: (path) => path.replace(/^\/api\/v1/, '/api/v1'),
+//         },
+//       },
+//     },
+//     define: {
+//       __APP_ENV__: JSON.stringify(env.VITE_ENV),
+//     },
+//   });
+// };
+
+
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return defineConfig({
@@ -11,14 +37,11 @@ export default ({ mode }) => {
       port: 5173,
       proxy: {
         '/api/v1': {
-          target: env.VITE_PROXY_ADDRESS,
+          target: 'k8s-default-reactapp-9a3b9418e9-8a3841d3a0aa2eaf.elb.ap-northeast-2.amazonaws.com',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api\/v1/, '/api/v1'),
         },
       },
-    },
-    define: {
-      __APP_ENV__: JSON.stringify(env.VITE_ENV),
     },
   });
 };
