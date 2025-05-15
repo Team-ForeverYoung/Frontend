2048a237aaab6f38c69c9e5ab01baee90f93aa95
woosungking <abuga991008@gmail.com>
2025-05-15 14:26:17 +0900

    chore

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+// import { defineConfig, loadEnv } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import tailwindcss from '@tailwindcss/vite';
 
 // export default ({ mode }) => {
 //   const env = loadEnv(mode, process.cwd(), '');
