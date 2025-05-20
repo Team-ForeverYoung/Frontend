ec6a79b4ed47a060a632f070912fed0e75e0f27b
woosungking <abuga991008@gmail.com>
2025-05-20 19:39:52 +0900

    feat: EventService 구현

---
+++ frontend/src/service/event/EventService.jsx
@@ -0,0 +1,12 @@
+import { getBaseInstance } from "../config";
+export const eventJoin = async (EventJoinResponseDto) => {
+  try {
+    const axiosInstance = getBaseInstance();
+    const response = await axiosInstance.post("/api/v1/event", EventJoinResponseDto);
+    console.log("이벤트 참여 성공");
+    return response.data;
+  } catch (error) {
+    console.error("이벤트 참여 실패", error);
+    throw error; 
+  }
+};
