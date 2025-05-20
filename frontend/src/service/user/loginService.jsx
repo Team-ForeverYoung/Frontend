1f5d4de9acb24e7f34b31ccdb702b05d3ed67be3
woosungking <abuga991008@gmail.com>
2025-05-20 18:40:59 +0900

    fix: axios 요청경로 수정

--- frontend/src/service/user/loginService.jsx
+++ frontend/src/service/user/loginService.jsx
@@ -3,7 +3,7 @@ import { getBaseInstance } from "../config";
 export const login = async (loginRequestDto) => {
   try {
     const axiosInstance = getBaseInstance();
-    const response = await axiosInstance.post('/login', loginRequestDto);
+    const response = await axiosInstance.post('/api/v1/user/login', loginRequestDto);
     return response.data;
   } catch (error) {
     console.error('로그인 실패:', error);
--- frontend/src/service/user/registerservice.jsx
+++ frontend/src/service/user/registerservice.jsx
@@ -4,7 +4,7 @@ import { OliveUserRegisterResponseDto } from "../../service/dto/OliveUserRespons
 export const oliveMemberRegister = async (oliveUserRegisterRequestDto) => {
   try {
     const baseInstance = getBaseInstance();
-    const response = await baseInstance.post("/join", oliveUserRegisterRequestDto,
+    const response = await baseInstance.post("/api/v1/user/join", oliveUserRegisterRequestDto,
       {
         headers: {
           "Content-Type": "application/json"
@@ -20,7 +20,7 @@ export const oliveMemberRegister = async (oliveUserRegisterRequestDto) => {
 
 export const oliveMemberInfo = async (oliveUserId) => {
     try {
-        const response = await baseInstance.get(`/user/${oliveUserId}`);
+        const response = await baseInstance.get(`/api/v1/user/${oliveUserId}`);
         const oliveUserInfoResponseDto = new OliveUserRegisterResponseDto(response.data.name, response.data.id, response.data.email, response.data.passwd, response.data.country);
         return response.data;
     } catch (error) {
